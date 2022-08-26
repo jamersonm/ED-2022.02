c3572149a3f856bc2750cced431b79f336aaf012
Jamal <102273220+Jamal4002@users.noreply.github.com>
2022-08-26 11:43:54 -0300

    Update Biblioteca01.c

--- Lista03/Biblioteca01.c
+++ Lista03/Biblioteca01.c
@@ -5,7 +5,7 @@ int VerificaMatrizNula(int m, int n, int mat[m][n])
 {
     for(int i = 0; i < m; i++)
     {
-        for(int j = 0; j < m; j++)
+        for(int j = 0; j < n; j++)
         {
             if(mat[i][j] != 0)
             {

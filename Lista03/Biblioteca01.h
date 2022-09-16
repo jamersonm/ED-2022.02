5007299fec9e3a38c6b72c19075587471e32315f
Jamal <102273220+Jamal4002@users.noreply.github.com>
2022-09-16 08:10:16 -0300

    Delete Lista03 directory

--- Lista03/Biblioteca01.h
+++
@@ -1,6 +0,0 @@
-#ifndef BIBLIOTECA01_H_INCLUDED
-#define BIBLIOTECA01_H_INCLUDED
-
-int VerificaMatrizNula(int m, int n, int mat[m][n]);
-
-#endif // BIBLIOTECA01_H_INCLUDED
--- Lista03/Biblioteca03.h
+++
@@ -1,8 +0,0 @@
-#ifndef BIBLIOTECA03_H_INCLUDED
-#define BIBLIOTECA03_H_INCLUDED
-
-int VerificaMatrizOposta(int m, int n, int matA[m][n], int matB[m][n]);
-
-int VerificaMatrizTransposta(int m, int n, int matA[m][n], int matB[n][m]);
-
-#endif // BIBLIOTECA03_H_INCLUDED

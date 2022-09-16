57d8eb417df913f156400b01396a2258f1bb504b
Jamal <102273220+Jamal4002@users.noreply.github.com>
2022-09-16 08:13:17 -0300

    Complete

---
+++ 3. Matrizes/Biblioteca03.c
@@ -0,0 +1,36 @@
+#include "Biblioteca03.h"
+
+int VerificaMatrizOposta(int m, int n, int matA[m][n], int matB[m][n])
+{
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(matA[i][j] - (2*matA[i][j]) != matB[i][j])
+                return 0;
+        }
+    }
+    return 1;
+}
+
+int VerificaMatrizTransposta(int m, int n, int matA[m][n], int matB[n][m])
+{
+    int transposta[n][m];
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            transposta[j][i] = matA[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            if(transposta[i][j] != matB[i][j])
+                return 0;
+        }
+    }
+    return 1;
+}
---
+++ 3. Matrizes/main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "Biblioteca01.h"
+#include "Biblioteca02.h"
+#include "Biblioteca03.h"
+
+void preencherMatriz(int m, int n, int mat[m][n])
+{
+    printf("\n======   PREENCHER MATRIZ   ======\n");
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            printf("Informe o valor para [%d][%d]: ", i+1, j+1);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+void exibirMatriz(int m, int n, int mat[m][n])
+{
+    printf("\n======   EXIBIR MATRIZ   ======\n");
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int lin, col;
+
+    printf("Informe o número de linhas da matriz: ");
+    scanf("%d", &lin);
+    printf("Informe o número de colunas da matriz: ");
+    scanf("%d", &col);
+
+    int matriz[lin][col];
+
+    preencherMatriz(lin, col, matriz);
+    exibirMatriz(lin, col, matriz);
+
+    printf("\n======   VERIFICAÇÕES   ======\n");
+    //MATRIZ NULA
+    printf("Matriz nula: %d \n",VerificaMatrizNula(lin, col, matriz));
+    if(lin == col)
+    {
+        //MATRIZ DIAGONAL
+        printf("Matriz diagonal: %d \n",VerificaMatrizDiagonal(lin, matriz));
+        //MATRIZ IDENTIDADE
+        printf("Matriz identidade: %d \n",VerificaMatrizIdentidade(lin, matriz));
+        //MATRIZ SIMÉTRICA
+        printf("Matriz simétrica: %d \n",VerificaMatrizSimetrica(lin, matriz));
+    }
+    else
+    {
+        printf("A matriz informada não é quadrada.\n Não é possível verificar diagonal, identidade e simetria.");
+    }
+
+    printf("\n======   MATRIZ OPOSTA   ======\n");
+    int linA, colA;
+    printf("Informe o número de linhas das matrizes: ");
+    scanf("%d", &linA);
+    printf("Informe o número de colunas das matrizes: ");
+    scanf("%d", &colA);
+    int matrizA[linA][colA];
+    int matrizB[linA][colA];
+    printf("\nPreencher matriz A");
+    preencherMatriz(linA, colA, matrizA);
+    printf("\nPreencher matriz B");
+    preencherMatriz(linA, colA, matrizB);
+    printf("\nMatriz A oposta à B: %d \n", VerificaMatrizOposta(linA, colA, matrizA, matrizB));
+
+    printf("\n======   MATRIZ TRANSPOSTA   ======\n");
+    int linC, colC;
+    printf("Informe o número de linhas das matrizes: ");
+    scanf("%d", &linC);
+    printf("Informe o número de colunas das matrizes: ");
+    scanf("%d", &colC);
+    int matrizC[linC][colC];
+    int matrizD[colC][linC];
+    printf("\nPreencher matriz C");
+    preencherMatriz(linC, colC, matrizC);
+    printf("\nPreencher matriz D");
+    preencherMatriz(colC, linC, matrizD);
+    printf("\nMatriz A transposta à B: %d \n", VerificaMatrizTransposta(linC, colC, matrizC, matrizD));
+
+    return 0;
+}

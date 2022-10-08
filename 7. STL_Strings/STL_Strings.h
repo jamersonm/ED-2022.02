50cfd9758766618657bfa8673805b2188c63f34e
Jamerson <102273220+jamersonm@users.noreply.github.com>
2022-10-07 22:33:04 -0300

    Complete

--- 7. STL_Strings/STL_Strings.h
+++ 7. STL_Strings/STL_Strings.h
@@ -7,26 +7,74 @@
 
 using namespace std;
 
-int QuantCaract(string str){
-    int i;
-    i=str.size();
-    return i;
+void InverteString(string &str)
+{
+    for (int i = 0; i < (int)str.size() / 2; i++)
+        swap(str[i], str[str.size() - i - 1]);
 }
 
-char *FoundCh(string str){
-    int s;
-    s= (signed int) str.find("ch");
-    //if(s<25){
-        //return s;
-    //}
-    return NULL;
+void AlteraIniciaisMaiusculas(string &str)
+{
+    str.at(0) = toupper(str.at(0));
+    for(int i = 2; i < str.size(); i++)
+    {
+        if(str.at(i-1) == ' ')
+            str.at(i) = toupper(str.at(i));
+    }
 }
 
-char *Copia(string *str1,string *str2){
-    str2=str1;
-    return str1;
+void MinusculaMaiuscula(string &str)
+{
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    AlteraIniciaisMaiusculas(str);
 }
 
+void cocatenar(string &str1, string &str2)
+{
+    str1 = str1 + str2;
+}
+
+int comparaStrings(string &str1, string &str2)
+{
+    return str1.compare(str2);
+}
+
+void IntercalaStrings(string &str1, string &str2, string &str3)
+{
+    int i = 0, j = 0, control = 0;
+    if(comparaStrings(str1, str2) >= 0)
+        str3 = str1 + str2;
+    else
+        str3 = str2 + str1;
+    do
+    {
+        if(i % 2 == 0)
+        {
+            str3.at(i) = str1.at(j);
+            j++;
+        }
+        i++;
+        if(i == str3.size() || j == str2.size())
+            control++;
+    }
+    while(j < str1.size());
+    i = 0;
+    j = 0;
+    control = 0;
+    do
+    {
+        if(i % 2 != 0)
+        {
+            str3.at(i) = str2.at(j);
+            j++;
+        }
+        i++;
+        if(i == str3.size() || j == str2.size())
+            control++;
+    }
+    while(control == 0);
+
+}
 
 
 
--- 7. STL_Strings/main.cpp
+++ 7. STL_Strings/main.cpp
@@ -7,23 +7,41 @@ using namespace std;
 
 int main()
 {
-    string str1,str2,s;
-    s="Arthur";
+    string str1("Cachorro");
+    cout << "Número de caracteres contidos: " << str1.size() <<  endl;
+    cout << "Posição da primeira ocorrência de \'ch\': " << str1.find("ch") << endl;
 
-    cout<<"Informe o conjunto de caracteres 1: "<<endl;
-    cin>>str1;
+    string str2;
+    str2.swap(str1);
+    cout << "Str2 copiada de str1: " << str2 << endl;
+    InverteString(str2);
+    cout << "Str2 invertida: " << str2 << endl;
 
-    cout<<"Informe o conjunto de caracteres 2: "<<endl;
-    cin>>str2;
-
-    cout<<" "<<QuantCaract(s)<<endl;
-    cout<<" "<<QuantCaract(s)<<endl;
-
-    cout<<"Posição de ch na string: "<<FoundCh(s)<<endl;
-
-    cout << "localizacao do primeiro caracter \'a\' " << s.find('A') << endl;
-    cout << "localizacao do primeiro caracter \'s\' " << s.find('r') << endl;
+    string str3("amanha tem jogo");
+    cout << "Str3: " << str3 << endl;
+    AlteraIniciaisMaiusculas(str3);
+    cout << "Str3 com as primeiras letras maiúsculas: " << str3 << endl;
+    string str4("AMANHA TEM JOGO");
+    cout << "Str4: " << str3 << endl;
+    MinusculaMaiuscula(str4);
+    cout << "Str4 com apenas as primeiras letras maiúsculas: " << str3 << endl;
 
 
+    str1 = "Cachorro";
+    cocatenar(str1, str2);
+    cout << "Str1 cocatenada a str2: " << str1 << endl;
+
+    string str5("cinco");
+    string str6("seis");
+    string str7;
+    IntercalaStrings(str5, str6, str7);
+    cout << "Intercalação entre str5 e str6: " << str7 << endl;
+
+    if(comparaStrings(str2, str1) == 0)
+        cout << "Str1 é igual a str2." << endl;
+    else if(comparaStrings(str1,  str2) < 0)
+        cout << "Str1 é maior do que str2." << endl;
+    else
+        cout << "Str1 é menor do que str2." << endl;
     return 0;
 }
